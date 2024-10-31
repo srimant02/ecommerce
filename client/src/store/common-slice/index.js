48e95b8f7773807f486e9c6e64bfe2d8fe33ec5a
srimant02 <srimantacharjya0203@gmail.com>
2024-10-31 19:09:35 +0530

    Update index.js

--- client/src/store/common-slice/index.js
+++ client/src/store/common-slice/index.js
@@ -10,7 +10,7 @@ export const getFeatureImages = createAsyncThunk(
   "/order/getFeatureImages",
   async () => {
     const response = await axios.get(
-      `https://mern-ecommerce-59xs.onrender.com/api/common/feature/get`
+      `https://ecommerce-8t0j.onrender.com/api/common/feature/get`
     );
 
     return response.data;
@@ -21,7 +21,7 @@ export const addFeatureImage = createAsyncThunk(
   "/order/addFeatureImage",
   async (image) => {
     const response = await axios.post(
-      `https://mern-ecommerce-59xs.onrender.com/api/common/feature/add`,
+      `https://ecommerce-8t0j.onrender.com/api/common/feature/add`,
       { image }
     );
 
