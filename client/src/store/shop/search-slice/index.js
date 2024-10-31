fcf64397c711a572ea9dc736e46dc83912ecdfd3
srimant02 <srimantacharjya0203@gmail.com>
2024-10-31 19:13:06 +0530

    Update index.js

--- client/src/store/shop/search-slice/index.js
+++ client/src/store/shop/search-slice/index.js
@@ -10,7 +10,7 @@ export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
     const response = await axios.get(
-      `https://mern-ecommerce-59xs.onrender.com/api/shop/search/${keyword}`
+      `https://ecommerce-8t0j.onrender.com/api/shop/search/${keyword}`
     );
 
     return response.data;
