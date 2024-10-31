4996a923e06099e1929389e7f05b1feb63206738
srimant02 <srimantacharjya0203@gmail.com>
2024-10-31 19:06:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const PORT = 5000;
 
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["https://ecommerce-8t0j.onrender.com"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
